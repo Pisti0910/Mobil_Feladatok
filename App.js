15ebbb9c5aa2b278c01df2416f66981ea3b1a252
Pisti0910 <gaal.istvan.1199@dszcbaross.edu.hu>
2024-11-11 08:29:59 +0100

    Rendezett dátum

--- App.js
+++ App.js
@@ -86,6 +86,7 @@ export default function App() {
         "datum":datum,
         "kesz":0
       })
+      uj.sort((a, b) => new Date(a.datum) - new Date(b.datum));
       setAdatTomb(uj)
       storeData(uj)
       alert("Sikeres felvitel!")
@@ -99,12 +100,12 @@ export default function App() {
   }
 
   const valtozikDatum=(event,datum)=>{
-    alert(datum)
+    //alert(datum)
     setShow(false)
-    setDatum(datum.getFullYear()+"."+(datum.getMonth()+1)+"."+datum.getDate())
+    setDatum(datum.getFullYear()+"-"+(datum.getMonth()+1)+"-"+datum.getDate())
   }
   const befejezVagyVissza=(id)=>{
-    alert(id)
+    //alert(id)
     let uj=[...adatTomb]
     for (elem of uj){
       if(elem.id==id){
